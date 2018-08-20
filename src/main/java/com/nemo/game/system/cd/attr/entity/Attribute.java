f1ec35bb6fdce2339075aa3fbd82da3055d973f6
quanmin <CaoYou123>
2018-08-20 09:22:43 +0800

    1

--- src/main/java/com/nemo/game/system/cd/attr/entity/Attribute.java
+++ src/main/java/com/nemo/game/system/cd/attr/entity/Attribute.java
@@ -1,10 +1,25 @@
 package com.nemo.game.system.cd.attr.entity;
 
+import io.protostuff.Tag;
 import lombok.extern.slf4j.Slf4j;
 
 //玩家属性
 @Slf4j
 public class Attribute implements Cloneable{
+    //生命值
+    @Tag(1)
+    protected int hp;
+    //魔法值
+    @Tag(2)
+    protected int mp;
+    //物攻
+    @Tag(3)
+    private int phyatk;
+    //物攻上限
+    @Tag(4)
+    private int phyatkmax;
+
+
 
 
 
